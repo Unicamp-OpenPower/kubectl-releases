fd4cc1144fe89129b78b6e1f37c498fee7a81516
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-10-02 20:06:27 -0300

    Create files for release

---
+++ build.sh
@@ -0,0 +1,26 @@
+github_version=$(cat github_version.txt)
+ftp_version=$(cat ftp_version.txt)
+del_version=$(cat delete_version.txt)
+
+if [ $github_version != $ftp_version ]
+then
+    mkdir -p $GOPATH/src/k8s.io
+    cd $GOPATH/src/k8s.io
+    wget https://github.com/kubernetes/kubernetes/archive/v$github_version.zip
+    unzip v$github_version.zip
+    mv kubernetes-$github_version/ kubernetes
+    cd kubernetes
+    make
+    cd _output/local/bin/linux/ppc64le/
+    ls
+    mv kubectl kubectl-$github_version
+    ./kubectl-$github_version --help
+
+    if [[ $github_version != $ftp_version ]]
+    then
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kubectl/latest kubectl-$github_version"
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kubectl/latest/kubectl-$ftp_version"
+    fi
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kubectl kubectl-$github_version"
+    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kubectl/kubectl-$del_version"
+fi
---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+github_version=$(cat github_version.txt)
+ftp_version=$(cat ftp_version.txt)
+LOCALPATH=$GOPATH/src/k8s.io/kubernetes/
+BINPATH=$GOPATH/src/k8s.io/kubernetes/_output/local/bin/linux/ppc64le
+
+if [ $github_version != $ftp_version ]
+then
+  git clone https://$USERNAME:$TOKEN@github.com/Unicamp-OpenPower/repository-scrips.git
+  cd repository-scrips/
+  chmod +x empacotar-deb.sh
+  chmod +x empacotar-rpm.sh
+  sudo mv empacotar-deb.sh $BINPATH
+  sudo mv empacotar-rpm.sh $BINPATH
+  cd $BINPATH
+  sudo ./empacotar-deb.sh kubectl kubectl-$github_version $github_version " "
+  sudo ./empacotar-rpm.sh kubectl kubectl-$github_version $github_version " " "kubectl: the command line util to talk to your cluster"
+  if [ $github_version > $ftp_version ]
+  then
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/debian/ppc64el/tool-kubernete/ $BINPATH/kubectl-$github_version-ppc64le.deb"
+    sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/rpm/ppc64le/tool-kubernete/ ~/rpmbuild/RPMS/ppc64le/kubectl-$github_version-1.ppc64le.rpm"
+  fi
+fi
